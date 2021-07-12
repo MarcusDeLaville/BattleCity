9b86fa37cc0d90f5b0a57be34e4c1a16e6e6ef40
Nikolai Menkov <nikolajmenkov@gmail.com>
2021-07-12 15:11:22 +0300

    Rename protected fields

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] protected float _moveSpeed = 2f;
+    [SerializeField] protected float MoveSpeed = 2f;
     
-    protected Rigidbody2D _rigidbody;
-    protected Vector2 _moveDirection;
+    protected Rigidbody2D Rigidbody;
+    protected Vector2 MoveDirection;
     
-    public Vector2 MoveDirection => _moveDirection;
+    public Vector2 MovementDirection => MoveDirection;
     
     public virtual void OnEnable()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        Rigidbody = GetComponent<Rigidbody2D>();
     }
 }
